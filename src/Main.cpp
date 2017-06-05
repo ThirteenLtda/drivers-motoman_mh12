16ce6e404d8fb571e0d4276dcb57def7bb639b1d
alcantara <alcantara@poli.ufrj.br>
2017-06-05 14:31:54 -0300

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <motoman_mh12/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    motoman_mh12::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
