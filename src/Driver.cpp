a2b893e2292045097b97250c0b43a27bcbd59fc6
alcantara <alcantara@poli.ufrj.br>
2017-06-21 15:18:46 -0300

    refactor to check separate msg type and length logic

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -28,96 +28,87 @@ void Driver::close()
   iodrivers_base::Driver::close();
 }
 
+int Driver::returnMsgSize(int msg_type) const
+{
+    switch(msg_type)
+    {
+        case MotomanMsgTypes::MOTOMAN_ROBOT_STATUS:
+            return MotomanMsgTypes::MOTOMAN_HEADER_MSG_SIZE + MotomanMsgTypes::MOTOMAN_ROBOT_STATUS_SIZE;
+        case MotomanMsgTypes::MOTOMAN_JOINT_FEEDBACK:
+            return MotomanMsgTypes::MOTOMAN_HEADER_MSG_SIZE + MotomanMsgTypes::MOTOMAN_JOINT_FEEDBACK_SIZE;
+        case MotomanMsgTypes::MOTOMAN_MOTION_REPLY:
+            return MotomanMsgTypes::MOTOMAN_HEADER_MSG_SIZE + MotomanMsgTypes::MOTOMAN_MOTION_REPLY_SIZE;
+        case MotomanMsgTypes::MOTOMAN_READ_SINGLE_IO_REPLY:
+            return MotomanMsgTypes::MOTOMAN_HEADER_MSG_SIZE + MotomanMsgTypes::MOTOMAN_READ_SINGLE_IO_REPLY_SIZE;
+        case MotomanMsgTypes::MOTOMAN_WRITE_SINGLE_IO_REPLY:
+            return MotomanMsgTypes::MOTOMAN_HEADER_MSG_SIZE + MotomanMsgTypes::MOTOMAN_WRITE_SINGLE_IO_REPLY_SIZE;
+        default:
+            return -1;
+    }
+}
+
+
 int Driver::extractPacket(uint8_t const* buffer, size_t buffer_size) const
 {
-  int32_t const* buffer_as_int32 = reinterpret_cast<int32_t const*>(buffer);
-  switch(buffer_as_int32[0])
-  {
-    case MotomanMsgTypes::MOTOMAN_ROBOT_STATUS:
-      if(buffer_size< MotomanMsgTypes::MOTOMAN_ROBOT_STATUS_SIZE)
-	return 0;
-      else
-	return MotomanMsgTypes::MOTOMAN_ROBOT_STATUS_SIZE;
-      
-    case MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL:
-      if(buffer_size<MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_MIN_SIZE)
-	return 0;
-      //check the buffer size for the different valid field options
-      switch(buffer_as_int32[2])
-	case 1: // time option
-	  if(buffer_size < MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_TIME_SIZE)
-	    return 0;
-	  else
-	    return MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_TIME_SIZE;
-	case 2: //position
-	  if(buffer_size < MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_POS_SIZE)
-	    return 0;
-	  else 
-	    return MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_POS_SIZE;
-	case 4: //velocity
-	  if(buffer_size < MotomanMsgTypes::MOTMAN_JOINT_TRAJ_PT_FULL_VEL_SIZE)
-	    return 0;
-	  else
-	    return MotomanMsgTypes::MOTMAN_JOINT_TRAJ_PT_FULL_VEL_SIZE;
-	case 8: //acceletarions
-	  if(buffer_size < MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_ACC_SIZE)
-	    return 0;
-	  else
-	    return MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_ACC_SIZE;
-	case 7: //All the fields are selected and it assumed to be set as default
-	  if(buffer_size < MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_ALL_SIZE)
-	    return 0;
-	  else
-	    return MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL_ALL_SIZE;
-	default:
-	  return -1;
-  }
+    int32_t const* buffer_as_int32 = reinterpret_cast<int32_t const*>(buffer);
+    int length = buffer_as_int32[0];
+    int msg_type = buffer_as_int32[1];
+    int expected_length = returnMsgSize(msg_type);
+    if(length>=expected_length)
+        return expected_length;
+    else
+        return 0;
+    
 }
 
 void Driver::parsePacket(uint8_t const* buffer, size_t size)
 {
-  int32_t const* buffer_as_int32 = reinterpret_cast<int32_t const*>(buffer);
-  switch(buffer_as_int32[0])
-  {
-    case MotomanMsgTypes::MOTOMAN_ROBOT_STATUS:
-      parseReadStatus(buffer,size);
-    case MotomanMsgTypes::MOTOMAN_JOINT_TRAJ_PT_FULL:
-      parseJointTrajPtFull(buffer,size);
-  }
+    int32_t const* buffer_as_int32 = reinterpret_cast<int32_t const*>(buffer);
+    switch(buffer_as_int32[1])
+    {
+        case MotomanMsgTypes::MOTOMAN_ROBOT_STATUS:
+            parseReadStatus(buffer,size);
+        case MotomanMsgTypes::MOTOMAN_JOINT_FEEDBACK:
+            parseJointFeedback(buffer,size);
+        case MotomanMsgTypes::MOTOMAN_MOTION_REPLY:
+            parseMotionReply(buffer,size);
+        case MotomanMsgTypes::MOTOMAN_READ_SINGLE_IO_REPLY:
+            parseReadSingleIOReply(buffer,size);
+    }
 }
 
 void Driver::parseReadStatus(uint8_t const* buffer, size_t size)
 {
-  msgs::StatusMessage const& msg = *reinterpret_cast<msgs::StatusMessage const*>(buffer);
-  msgs::MotomanStatus status;
-  if (msg.drives_powered == -1)
-    throw std::runtime_error("robot drivers in unknown state");
-  else 
-    status.drives_powered = bool(msg.drives_powered);
-  if(msg.e_stopped == -1)
-    throw std::runtime_error("e-stop controller in unkown state");
-  else
-    status.e_stopped = bool(msg.e_stopped);
-  if(msg.error_code == -1)
-    throw std::runtime_error("alarms in unkown state");
-  else
-    status.error_code = int(msg.error_code);
-  if(msg.ln_error == -1)
-    throw std::runtime_error("alarms in unkown state");
-  else 
-    status.ln_error = bool(msg.ln_error);
-  if(msg.ln_motion == -1)
-    throw std::runtime_error("controller in unkown state");
-  else
-    status.ln_motion = bool(msg.ln_motion);
-  if(msg.mode == -1)
-    throw std::runtime_error("controller mode is unknown");
-  else 
-    status.mode = bool(msg.mode);
-  if(msg.motion_possible == -1)
-    throw std::runtime_error("ROCK communication state is unknown");
-  else
-    status.motion_possible = bool(msg.motion_possible);
+    msgs::StatusMsg const& msg = *reinterpret_cast<msgs::StatusMsg const*>(buffer);
+    msgs::MotomanStatus status;
+    if (msg.drives_powered == -1)
+        throw std::runtime_error("robot drivers in unknown state");
+    else 
+        status.drives_powered = bool(msg.drives_powered);
+    if(msg.e_stopped == -1)
+        throw std::runtime_error("e-stop controller in unkown state");
+    else
+        status.e_stopped = bool(msg.e_stopped);
+    if(msg.error_code == -1)
+        throw std::runtime_error("alarms in unkown state");
+    else
+        status.error_code = int(msg.error_code);
+    if(msg.ln_error == -1)
+        throw std::runtime_error("alarms in unkown state");
+    else 
+        status.ln_error = bool(msg.ln_error);
+    if(msg.ln_motion == -1)
+        throw std::runtime_error("controller in unkown state");
+    else
+        status.ln_motion = bool(msg.ln_motion);
+    if(msg.mode == -1)
+        throw std::runtime_error("controller mode is unknown");
+    else 
+        status.mode = bool(msg.mode);
+    if(msg.motion_possible == -1)
+        throw std::runtime_error("ROCK communication state is unknown");
+    else
+        status.motion_possible = bool(msg.motion_possible);
 }
 
 void Driver::parseJointTrajPtFull(uint8_t const* buffer, size_t size)
--- src/Driver.hpp
+++ src/Driver.hpp
@@ -6,26 +6,28 @@
 
 namespace motoman_mh12
 {
-  class Driver : public iodrivers_base::Driver
-  {
+    class Driver : public iodrivers_base::Driver
+    {
         std::vector<uint8_t> buffer;
-
+        
     public:
         Driver();
-
-       void open(std::string const& uri);
-
+        
+        void open(std::string const& uri);
+        
         /** Read available packets on the I/O */
         void read();
-	
-	void close();
-	int extractPacket (uint8_t const *buffer, size_t buffer_size) const;
-	void parsePacket(uint8_t const* buffer, size_t size);
-	void parseReadStatus(uint8_t const* buffer, size_t size);
-	void parseJointFeedback(uint8_t const* buffer, size_t size);
-    void parseMotionReply(uint8_t const* buffer, size_t size);
-    void parseReadSingleIOReply(uint8_t const* buffer, size_t size);
-   };
+        
+        void close();
+        int extractPacket (uint8_t const *buffer, size_t buffer_size) const;
+        
+        int returnMsgSize(int msg_type) const;
+        void parsePacket(uint8_t const* buffer, size_t size);
+        void parseReadStatus(uint8_t const* buffer, size_t size);
+        void parseJointFeedback(uint8_t const* buffer, size_t size);
+        void parseMotionReply(uint8_t const* buffer, size_t size);
+        void parseReadSingleIOReply(uint8_t const* buffer, size_t size);
+    };
 }
 
 #endif
