fd7737232369e399affdea110507cb9bae8d2ec4
alcantara <alcantara@poli.ufrj.br>
2017-06-27 16:57:12 -0300

    add writeSingleIO msgs

--- src/Driver.hpp
+++ src/Driver.hpp
@@ -23,7 +23,6 @@ namespace motoman_mh12
         msgs::MotomanStatus parseReadStatus(uint8_t const* buffer, size_t size) const;
         msgs::MotomanJointFeedback parseJointFeedback(uint8_t const* buffer) const;
         void parseMotionReply(uint8_t const* buffer);
-        void parseReadSingleIOReply(uint8_t const* buffer);
         bool waitForReply(base::Time const& timeout, int32_t msg_type);
         void sendJointTrajPTFullCmd(int robot_id, int sequence, base::Time timestamp,
                                     std::vector<base::JointState> joint_states);
@@ -32,8 +31,15 @@ namespace motoman_mh12
         void sendMotionCtrl(int robot_id, int sequence, int cmd);
         void readMotionCtrlReply(base::Time const& timeout);
         
-        void queryReadSingleIO(int IOaddress);
+        void sendReadSingleIO(int IOaddress);
         void readSingleIOReply(const base::Time& timeout);
+        void parseReadSingleIOReply(uint8_t const* buffer);
+
+        
+        void sendWriteSingleIo(int IOaddress, int value);
+        void readWriteSingleIO(const base::Time& timeout);
+        void parseWriteSingleIOReply(uint8_t const* buffer);
+
         
         msgs::MotomanStatus status;
         msgs::MotomanJointFeedback joint_feedback;
