8be2cc2136f7c475f87af8fa31a6df4a6d763c8e
Gabriel Alcantara <alcantara@poli.ufrj.br>
2017-08-21 15:05:59 -0300

    add sendJointTrajPTFullCmd unit test

--- test/test_driver_mh12.cpp
+++ test/test_driver_mh12.cpp
@@ -9,7 +9,6 @@ using namespace motoman_mh12;
 
 int main(int argc, char **argv)
 {
-
   Driver driver_ctrl;
   driver_ctrl.openTCP("192.168.10.77", 50240);
   
@@ -23,45 +22,47 @@ int main(int argc, char **argv)
        msgs::MotomanMsgType msg_type =  driver_streaming.read();
        if(msg_type == msgs::MOTOMAN_JOINT_FEEDBACK)
        {
-            msgs::MotomanJointFeedback joint_feedback = driver_streaming.getJointFeedback();
-            current_position = joint_feedback.joint_states;
-            std::cout << "Current Position aquired" << std::endl;
-            break;
+           msgs::MotomanJointFeedback joint_feedback = driver_streaming.getJointFeedback();
+           current_position = joint_feedback.joint_states;
+           std::cout << "Current Position aquired" << std::endl;
+           std::cout << "Robot id: " << joint_feedback.robot_id << std::endl;
+           std::cout << "Joint 0: " << joint_feedback.joint_states[0].position << std::endl;
+           std::cout << "Joint 1: " << joint_feedback.joint_states[1].position << std::endl;
+           std::cout << "Joint 2: " << joint_feedback.joint_states[2].position << std::endl;
+           std::cout << "Joint 3: " << joint_feedback.joint_states[3].position << std::endl;
+           std::cout << "Joint 4: " << joint_feedback.joint_states[4].position << std::endl;
+           std::cout << "Joint 5: " << joint_feedback.joint_states[5].position << std::endl;
+           std::cout << "Joint 6: " << joint_feedback.joint_states[6].position << std::endl;
+           std::cout << "Joint 7: " << joint_feedback.joint_states[7].position << std::endl;
+           std::cout << "Joint 8: " << joint_feedback.joint_states[8].position << std::endl;
+           std::cout << "Joint 9: " << joint_feedback.joint_states[9].position << std::endl;
+           std::cout << "TIME: " << joint_feedback.time.toSeconds() << std::endl;
+           break;
        }
   }
        
   msgs::MotionReply reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::START_TRAJ_MODE);
-  std::cout << "Result: " << reply.result << " for the cmd: " << reply.command << std::endl;
+  reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::CHECK_MOTION_READY);
   
-  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 0, base::Time::now(), current_position);
+  for(int i=0; i<current_position.size();i++)
+  {
+      current_position[i].speed = 0.0;
+      current_position[i].acceleration = 0.0;
+  }
+  
+  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 0, base::Time::fromSeconds(0), current_position);
   std::cout << "Result traj: " << reply.result << " for the cmd: " << reply.command << std::endl;
   std::cout << "SubCode: " << reply.subcode << std::endl;
   
-  current_position[3].position = 0.9;
-  current_position[3].speed = 0.2;
-  current_position[3].acceleration = 50;
+  current_position[0].position = 1.57;
   
-  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 1, base::Time::now(), current_position);
-  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 2, base::Time::now(), current_position);
-  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 3, base::Time::now(), current_position);
-  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 4, base::Time::now(), current_position);
-  std::cout << "Result traj: " << reply.result << " for the cmd: " << reply.command << std::endl;
-  std::cout << "SubCode: " << reply.subcode << std::endl;
-  
-  
-  current_position[3].position = 0.9;
-  current_position[3].speed = 0.0;
-  current_position[3].acceleration = 0;
-  
-  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 5, base::Time::now(), current_position);
+  reply = driver_ctrl.sendJointTrajPTFullCmd(0, 1, base::Time::fromSeconds(5), current_position);
   std::cout << "Result traj: " << reply.result << " for the cmd: " << reply.command << std::endl;
   std::cout << "SubCode: " << reply.subcode << std::endl;
  
-  reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::CHECK_MOTION_READY);
+  //reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::STOP_MOTION);
   std::cout << "Result: " << reply.result << " for the cmd: " << reply.command << std::endl;
-  reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::STOP_TRAJ_MODE);
-  std::cout << "Result: " << reply.result << " for the cmd: " << reply.command << std::endl;
-  reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::STOP_MOTION);
+  //reply = driver_ctrl.sendMotionCtrl(0, 0, msgs::motion_ctrl::MotionControlCmds::STOP_TRAJ_MODE);
   std::cout << "Result: " << reply.result << " for the cmd: " << reply.command << std::endl;
         
 }
